0f4d95c135dcbb90f69677cb75da57a77fd8a246
shanmukhmanogna <78977617+shanmukhmanogna@users.noreply.github.com>
2021-08-11 19:46:25 -0400

    Create LexicalScoping.R

---
+++ LexicalScoping.R
@@ -0,0 +1,32 @@
+## Write a short comment describing this function
+## the function below creates a special object that caches the matrix and 
+## its inverse. The function returns a list containing functions to set 
+
+makeCacheMatrix <- function(x = matrix(sample(1:100,9),3,3)) {
+  s <- NULL
+  set <- function(y) {
+    x <<- y
+    s <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) s <<- solve
+  getsolve <- function() s
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
+}
+
+
+## changing "mean" to "solve" and "m" to "s"
+
+cacheSolve <- function(x, ...) {
+  s <- x$getsolve()
+  if(!is.null(s)) {
+    message("getting inversed matrix")
+    return(s)
+  }
+  data <- x$get()
+  s <- solve(data, ...)
+  x$setsolve(s)
+  s
+}
