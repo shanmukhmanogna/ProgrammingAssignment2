fbd6d006dcd466a24d1d9637a2329de27a45195a
shanmukhmanogna <78977617+shanmukhmanogna@users.noreply.github.com>
2021-08-14 13:58:23 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,36 +1,33 @@
-## Put comments here that give an overall description of what your
 ## functions do
-
 ## Write a short comment describing this function
+##Caches the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-  s <- NULL
-  set <- function(y) {
-    x <<- y
-    s <<- NULL
+  invertible <- NULL
+  set <- function(z) {              ##Sets value of matrix with another function
+    x <<- z
+    invertible <<- NULL             ##Variables at different levels
   }
-  get <- function() x
-  setsolve <- function(solve) s <<- solve
-  getsolve <- function() s
-  list(set = set, get = get,
-       setsolve = setsolve,
-       getsolve = getsolve)
+  
+  get <- function() {x}             ##Gets value of matrix
+  setInverse <- function(inverse) {invertible <<- inverse}   ##Sets value of the inverse
+  getInverse <- function() {invertible}                      ##Retrieves^
+  list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)  
 }
-
 
 
 ## Write a short comment describing this function
-
+##Gets data from cache
 cacheSolve <- function(x, ...) {
-  s <- x$getsolve()
-  if(!is.null(s)) {
-    message("getting inversed matrix")
-    return(s)
-  }
-  data <- x$get()
-  s <- solve(data, ...)
-  x$setsolve(s)
-  s
-}
-
-## Return a matrix that is the inverse of 'x'
+        ## Return a matrix that is the inverse of 'x'
+  invertible <- x$getInverse()      ##Gets inverse of x and assigns it to the variable
+  if(!is.null(invertible)) {        ##If the inverse was already found, it takes it from the cache
+    message("Getting Info In Cache...")
+    return(invertible)
+   }     
+  
+  mat <- x$get()                    ##Gets inverse of matrix and sets value, if not done already
+  invertible <- solve(mat, ...)
+  x$setInverse(invertible)
+  invertible                        ##Prints
+ }
